3c49444bd3779d5f804dc566046a68719d5ed7ad
Anitachi <Chikaanita750@gmail.com>
2022-10-05 03:32:20 -0700

    added files

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let theLines = 'C is fun';
+let secondLine = 'Python is cool';
+let thirdLine = 'JavaScript is amazing';
+console.log(theLines);
+console.log(secondLine);
+console.log(thirdLine);
